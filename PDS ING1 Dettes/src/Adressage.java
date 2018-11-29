249549ccbf2dc6adda6341d6472177f8b3b34d9f
hatim.laarabi <44162280+hatim-laarabi@users.noreply.github.com>
2018-11-29 20:24:26 +0100

    Add files via upload
    Premier commit. Serveur + client

---
+++ PDS ING1 Dettes/src/Adressage.java
@@ -0,0 +1,24 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+
+public class Adressage {
+
+    public static void main(String[] args) {
+
+        InetAddress LocaleAdresse ;
+        InetAddress ServeurAdresse;
+
+        try {
+
+            LocaleAdresse = InetAddress.getLocalHost();
+            System.out.println("L'adresse locale est : "+LocaleAdresse );
+
+
+        } catch (UnknownHostException e) {
+
+            e.printStackTrace();
+        }
+    }
+
+}
